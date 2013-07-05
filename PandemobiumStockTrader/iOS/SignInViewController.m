06b1d4b39369ebde5c266e362e9f69ee7a640ab8
asalazar7 <asalazar@denimgroup.com>
2013-07-05 11:36:57 -0500

    Added login information

--- PandemobiumStockTrader/iOS/SignInViewController.m
+++ PandemobiumStockTrader/iOS/SignInViewController.m
@@ -51,15 +51,71 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
 - (IBAction)loginButtonPressed:(UIButton *)sender
 {
     NSLog(@"loginwas pressed");
-    //do stuff
+    NSError *error;
     
-    if(self.rememberloginSwitch.on){
-        NSLog(@"SET TO REMEMBER LOGIN");
-    }else{
-        NSLog(@"not set to remmember your login");
+    NSString *url = [[NSString alloc]initWithFormat:@"http://localhost:8080/user.jsp?method=logIn&username=%@&password=%@", usernameText.text, passwordText.text];
+    url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSData *responseData = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
+    NSDictionary * firstParse = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+    NSArray *secondParse = [firstParse objectForKey:@"Results"];
+    NSDictionary *results = [secondParse objectAtIndex:0];
+    NSInteger userID = [[results valueForKey:@"userID"] intValue];
+    
+    usernameText.text = @"";
+    passwordText.text = @"";
+    UIAlertView *alert;
+    if(userID >= 1)
+    {
+        NSString *message = [[NSString alloc] initWithFormat:@"Welcome back %@", [results objectForKey:@"userName"]];
+        alert = [[UIAlertView alloc] initWithTitle:@"Welcome"
+                                            message:message
+                                            delegate:nil
+                                            cancelButtonTitle:@"OK"
+                                            otherButtonTitles:nil];
+        [alert show];
+        if(self.rememberloginSwitch.on)
+        {
+            //TO DO: Write to local file
+            NSLog(@"SET TO REMEMBER LOGIN");
+        }else
+        {
+            NSLog(@"not set to remmember your login");
+        }
     }
+    else
+    {
+        NSString *message = @"Invalid Username or Password";
+        alert = [[UIAlertView alloc] initWithTitle:@"Invalid"
+                                           message:message
+                                         delegate:nil
+                                 cancelButtonTitle:@"Try Again"
+                                 otherButtonTitles:nil];
+        [alert show];
+
+        
+    }
+   
+    
+    //To Do: Take to Quotes from here. 
+    
+    
     
 }
+
+//-(void) fetchData
+//{
+//    
+//    NSString *url = [[NSString alloc]initWithFormat:@"http://localhost:8080/user.jsp?method=logIn&username=user&password=password"];
+//    
+//    NSError *error;
+//    NSData *responseData = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
+//    
+//    NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+//    NSDictionary *query = [jsonData objectForKey:@"query"];
+//    NSDictionary *results = [query objectForKey:@"results"];
+//    stockInfo = [results objectForKey:@"quote"];
+//    
+//}
 
 //for handling input text and keybaord behavior
 - (void)textFieldDidBeginEditing:(UITextField *)textField
