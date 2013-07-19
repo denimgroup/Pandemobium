19cbffa0f45a9a7d6e751178c84c2eb0b1d20372
Thomas-Eisen <tsalazar@denimgroup.com>
2013-07-19 14:58:33 -0500

    updating with manage tips view controller

---
+++ PandemobiumStockTrader/iOS/PandemobiumV2/Controllers/ManageTipsViewController.h
@@ -0,0 +1,26 @@
+//
+//  ManageTipsViewController.h
+//  Pandemobium
+//
+//  Created by Thomas Salazar on 7/18/13.
+//  Copyright (c) 2013 Thomas Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
+#import "AppDelegate.h"
+#import "DBHelper.h"
+#import "SVProgressHUD.h"
+#import "DBHTTPClient.h"
+
+@interface ManageTipsViewController : UIViewController <UITextFieldDelegate>
+- (IBAction)revealMenu:(id)sender;
+- (void)textFieldDidBeginEditing:(UITextField *)textField;
+- (void)textFieldDidEndEditing:(UITextField *)textField;
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;
+@property (strong, nonatomic) IBOutlet UITextView *reason;
+@property (strong, nonatomic) IBOutlet UITextField *symbol;
+- (IBAction)submitReason:(id)sender;
+- (IBAction)clearTextView:(id)sender;
+@end
---
+++ PandemobiumStockTrader/iOS/PandemobiumV2/Controllers/ManageTipsViewController.m
@@ -0,0 +1,142 @@
+//
+//  ManageTipsViewController.m
+//  Pandemobium
+//
+//  Created by Thomas Salazar on 7/18/13.
+//  Copyright (c) 2013 Thomas Salazar. All rights reserved.
+//
+
+#import "ManageTipsViewController.h"
+@interface ManageTipsViewController () <UITextFieldDelegate>
+
+@end
+
+@implementation ManageTipsViewController
+
+CGFloat animatedDistance;
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
+static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
+
+@synthesize reason;
+@synthesize symbol;
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)revealMenu:(id)sender
+{
+    [self.slidingViewController anchorTopViewTo:ECRight];
+}
+
+- (IBAction)submitReason:(id)sender
+{
+    NSLog(@"submit reason is pressed");
+    
+}
+- (IBAction)clearTextView:(id)sender
+{
+    NSLog(@"clear text button is pressed");
+    self.symbol.text = @"";
+    self.reason.text = @"Reason: ";
+}
+
+//for handling input text and keybaord behavior
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    CGRect textFieldRect = [self.view.window convertRect:textField.bounds fromView:textField];
+    
+    CGRect viewRect = [self.view.window convertRect:self.view.bounds fromView:self.view];
+    
+    CGFloat midline= textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
+    
+    CGFloat numerator = midline - viewRect.origin.y -MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    
+    CGFloat denominator = (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION) * viewRect.size.height;
+    
+    CGFloat heightFraction = numerator / denominator;
+    
+    if(heightFraction < 0.0)
+    {
+        heightFraction = 0.0;
+    }
+    else if(heightFraction > 1.0)
+    {
+        heightFraction = 1.0;
+    }
+    
+    
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    if(orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    }
+    else
+    {
+        animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+    }
+    
+    CGRect viewFrame= self.view.frame;
+    viewFrame.origin.y -= animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    [UIView commitAnimations];
+}
+
+-  (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+	if([text isEqualToString:@"\n"])
+	{
+		[textView resignFirstResponder];
+		return NO;
+	}
+	return YES;
+}
+@end
